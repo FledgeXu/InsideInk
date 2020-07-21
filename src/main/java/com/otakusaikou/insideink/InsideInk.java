cea530b26488d99b46faf0bbad12ca1b2c6c9b61
FledgeXu <xzk0701@gmail.com>
2020-07-21 19:31:39 +0800

    Mod Info init.

---
+++ src/main/java/com/otakusaikou/insideink/InsideInk.java
@@ -0,0 +1,7 @@
+package com.otakusaikou.insideink;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(Utils.MOD_ID)
+public class InsideInk {
+}
---
+++ src/main/java/com/otakusaikou/insideink/Utils.java
@@ -0,0 +1,5 @@
+package com.otakusaikou.insideink;
+
+public class Utils {
+    public static final String MOD_ID = "insideink";
+}
